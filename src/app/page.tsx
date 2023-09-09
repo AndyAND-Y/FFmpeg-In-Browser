51d2592da7d6a956cb703cabb2d75334efd42ce4
Andy <andreislatinaru@gmail.com>
2023-09-09 22:19:55 +0100

    time states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,65 @@ import { fetchFile, toBlobURL } from '@ffmpeg/util';
 import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-    const [loaded, setLoaded] = useState(false);
+
+    const [progress, setProgress] = useState(0);
+    const [timeTaken, setTimeTaken] = useState(0);
+    const [logMessages, setLogMessages] = useState<string[]>([]);
+
+
+    const [loadedFFMPEG, setLoadedFFMPEG] = useState(false);
     const ffmpegRef = useRef(new FFmpeg());
-    const videoRef = useRef<HTMLVideoElement | null>(null);
+
     const inputRef = useRef<HTMLInputElement | null>(null);
 
+    const handleProgressUpdate = ({ progress, time }: { progress: number, time: number }) => {
+        setProgress(progress);
+    }
+
+    const handleLogMessage = ({ type, message }: { type: string, message: string }) => {
+        setLogMessages((prev) => [...prev, message])
+
+        const patternTime = /time=([\d:.]+)/
+        const patternSpeed = /speed=([\d.]+)/
+
+        const time = message.match(patternTime)?.[1];
+        const speed = message.match(patternSpeed)?.[1];
+
+        console.log("speed: ", Number(speed));
+        console.log("time: ", time);
+
+        if (time === undefined || speed === undefined) return;
+
+        const timestamp = time.split(":").reverse()
+            .map((el, index) => {
+                console.log("el", el);
+                const value = Number(el) * Math.pow(60, index);
+                console.log("value: ", value);
+                return value;
+            })
+            .reduce((acc, curent) => {
+                return acc + curent;
+            }, 0)
+
+        console.log("ts: ", timestamp);
+
+        setTimeTaken(timestamp / Number(speed));
+
+    }
+
+
     useEffect(() => {
+
+        const ffmpeg = ffmpegRef.current;
+        ffmpeg.on('log', handleLogMessage);
+        ffmpeg.on('progress', handleProgressUpdate);
         load();
+
+        return () => {
+            ffmpeg.off("log", handleLogMessage);
+            ffmpeg.off("progress", handleProgressUpdate);
+        }
+
     }, [])
 
     const load = async () => {
@@ -20,51 +72,54 @@ export default function Home() {
         // toBlobURL is used to bypass CORS issue, urls with the same
         // domain can be used directly.
 
-        ffmpeg.on('log', ({ type, message }: { type: string, message: string }) => {
-            console.log(message);
-        })
-
-        ffmpeg.on('progress', ({ progress, time }: { progress: any, time: any }) => {
-            console.log(progress, time);
-        })
-
         await ffmpeg.load({
             coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
             wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
         });
-        setLoaded(true);
+        setLoadedFFMPEG(true);
     }
 
     const transcode = async () => {
 
-        if (!inputRef.current || !inputRef.current.files || !videoRef.current) return;
+        if (!inputRef.current || !inputRef.current.files) return;
 
         const file = inputRef.current.files[0];
 
         const ffmpeg = ffmpegRef.current;
         await ffmpeg
             .writeFile(
-                'input.webm',
+                'input.mp4',
                 await fetchFile(file)
             );
-        await ffmpeg.exec(['-i', 'input.webm', 'output.mp4']);
+        await ffmpeg.exec(['-i', 'input.mp4', 'output.mp4']);
         const data = await ffmpeg.readFile('output.mp4') as Uint8Array
-        videoRef.current.src =
-            URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
     }
 
-    return (loaded
-        ? (
-            <>
-                <video ref={videoRef} controls></video><br />
+    if (!loadedFFMPEG) {
+        return <div> Loading... ~31Mb </div>
+    }
+
+    return (
+        <>
+            <div
+                className='flex flex-col justify-center items-center'
+            >
                 <input ref={inputRef} type="file" />
-                <button onClick={transcode}>Transcode webm to mp4</button>
-                <p>Open Developer Tools (Ctrl+Shift+I) to View Logs</p>
-            </>
-        )
-        : (
-            <button onClick={load}>Load ffmpeg-core (~31 MB)</button>
-        )
+                <button onClick={transcode}>Compute!</button>
+
+                <div className='h-24 overflow-y-auto p-1 border w-1/2'>
+                    {logMessages.map((message, index) => {
+                        return <p key={index}> {message} </p>
+                    })}
+                </div>
+
+                <div>
+                    <p>Time Taken: {timeTaken.toFixed(2) + 's'}</p>
+                    <p>Time Remaning: {((100 / (progress * 100) - 1) * timeTaken).toFixed(2) + 's'}</p>
+                    <p>Progress: {(progress * 100).toFixed(2) + '%'}</p>
+                </div>
+            </div>
+        </>
     );
 }
 
